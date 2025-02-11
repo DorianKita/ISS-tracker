97d2e6ab10b8a0e0310238338c8be33e5b682dcc
Dorian Kita <dorian.kita90@gmail.com>
2025-02-11 20:31:25 +0100

    created is_overhead function to check whether

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import requests
 import datetime
 
-# url = "http://api.open-notify.org/iss-now.json"
-#
-# response = requests.get(url=url)
-# response.raise_for_status()
-#
-# data = response.json()
-# longitude = data['iss_position']['longitude']
-# latitude = data['iss_position']['latitude']
-#
-# iss_position = (longitude, latitude)
-# print(iss_position)
+MY_LAT= 51.110548
+MY_LNG= 17.025558
+
+
+def is_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    longitude = float(data['iss_position']['longitude'])
+    latitude = float(data['iss_position']['latitude'])
+    iss_position = (longitude, latitude)
+
+    if MY_LAT - 5 <= latitude <= MY_LAT +5 and MY_LNG - 5 <= longitude <= MY_LNG +5:
+        return True
+    else:
+        return False
 
 now = datetime.datetime.now()
 
 parameters = {
-    'lat': 51.110548,
-    'lng': 17.025558,
+    'lat': MY_LAT,
+    'lng': MY_LNG,
     'formatted': 0
 }
 
 response = requests.get('https://api.sunrise-sunset.org/json', params=parameters,)
 response.raise_for_status()
 data = response.json()
-sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
-sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+sunrise = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+sunset = int(data['results']['sunset'].split('T')[1].split(':')[0])
+
+is_overhead()
