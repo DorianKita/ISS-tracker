06642c59b6acfef683e124f27eb65c2ca158d977
Dorian Kita <dorian.kita90@gmail.com>
2025-02-11 20:10:21 +0100

    extracted time from api and datetime module to compare them

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import requests
+import datetime
 
-url = "http://api.open-notify.org/iss-now.json"
+# url = "http://api.open-notify.org/iss-now.json"
+#
+# response = requests.get(url=url)
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
 
-response = requests.get(url=url)
+now = datetime.datetime.now()
+
+parameters = {
+    'lat': 51.110548,
+    'lng': 17.025558,
+    'formatted': 0
+}
+
+response = requests.get('https://api.sunrise-sunset.org/json', params=parameters,)
 response.raise_for_status()
-
 data = response.json()
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
-
-iss_position = (longitude, latitude)
-print(iss_position)
+sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = data['results']['sunset'].split('T')[1].split(':')[0]
